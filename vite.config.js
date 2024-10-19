ed9f71a820f0a64f5335c4fa77fbaba16645d1e2
Keshav Gulati <keshavgulati@Keshavs-MacBook-Air.local>
2024-10-19 12:10:37 +0530

    First commit

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // Proxy for Restaurant List API
-      "/dapi/restaurants/list": {
-        target: "https://www.swiggy.com",
-        changeOrigin: true,
-        rewrite: (path) =>
-          path.replace(/^\/dapi\/restaurants\/list/, "/dapi/restaurants/list"),
-      },
-      // Proxy for Menu API
-      "/dapi/menu/pl": {
-        target: "https://www.swiggy.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/dapi\/menu\/pl/, "/dapi/menu/pl"),
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     // Proxy for Restaurant List API
+  //     "/dapi/restaurants/list": {
+  //       target: "https://www.swiggy.com",
+  //       changeOrigin: true,
+  //       rewrite: (path) =>
+  //         path.replace(/^\/dapi\/restaurants\/list/, "/dapi/restaurants/list"),
+  //     },
+  //     // Proxy for Menu API
+  //     "/dapi/menu/pl": {
+  //       target: "https://www.swiggy.com",
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/dapi\/menu\/pl/, "/dapi/menu/pl"),
+  //     },
+  //   },
+  // },
 });
